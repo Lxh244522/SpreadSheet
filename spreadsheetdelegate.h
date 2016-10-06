8055fdf4fda349f5a2fd5bea3448283ceb0dcc66
Troy <2445224178@qq.com>
2016-10-06 14:34:23 +0800

    add item and delegate file

---
+++ spreadsheetdelegate.h
@@ -0,0 +1,23 @@
+#ifndef SPREADSHEETDELEGATE_H
+#define SPREADSHEETDELEGATE_H
+#include <QItemDelegate>
+
+#include "spreadsheet.h"
+
+class SpreadSheetDelegate : public QItemDelegate
+{
+    Q_OBJECT
+
+public:
+    SpreadSheetDelegate(QObject *parent = 0);
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &,
+                          const QModelIndex &index) const Q_DECL_OVERRIDE;
+    void setEditorData(QWidget *editor, const QModelIndex &index) const Q_DECL_OVERRIDE;
+    void setModelData(QWidget *editor, QAbstractItemModel *model,
+                      const QModelIndex &index) const Q_DECL_OVERRIDE;
+
+private slots:
+    void commitAndCloseEditor();
+};
+
+#endif // SPREADSHEETDELEGATE_H
